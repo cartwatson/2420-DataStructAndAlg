f756825a5917589b5346d98c1257be1de7077844
cartwatson <cartwatson@gmail.com>
2022-11-12 17:59:25 -0700

    simplified duplicate code

--- assign5-hexGame/HexGame.java
+++ assign5-hexGame/HexGame.java
@@ -20,10 +20,10 @@ public class HexGame {
             colorArrayList.add(Color.NONE);
         }
         // init edges
-        TOP_EDGE    = gameSize - 4; // 122
+        TOP_EDGE = gameSize - 4; // 122
         BOTTOM_EDGE = gameSize - 3; // 123
-        LEFT_EDGE   = gameSize - 2; // 124
-        RIGHT_EDGE  = gameSize - 1; // 125
+        LEFT_EDGE = gameSize - 2; // 124
+        RIGHT_EDGE = gameSize - 1; // 125
         // color edges
         colorArrayList.set(TOP_EDGE, Color.RED);
         colorArrayList.set(BOTTOM_EDGE, Color.RED);
@@ -31,36 +31,15 @@ public class HexGame {
         colorArrayList.set(RIGHT_EDGE, Color.BLUE);
     }
 
-    // turn for blue
     public boolean playBlue(int position, boolean displayNeighbors) {
-        // position is an integer for the available hex positions
-        if (position < 1 || position > (size * size)) {
-            return false;
-        }
-        // this is for debugging only
-        if (displayNeighbors) {
-            // the hex positions of neighboring cell are displayed, occupied or not
-            System.out.println(getNeighborsBlue(position));
-        }
-        // logic for playing
-        if (this.colorOfOccupied(position) == Color.NONE) {
-            colorArrayList.set(position, Color.BLUE);
-            ArrayList<Integer> neighbors = getNeighborsBlue(position);
-            for (Integer neighbor : neighbors) {
-                // only union if neighbor is blue
-                if (this.colorOfOccupied(neighbor) == Color.BLUE) {
-                    set.union(position, neighbor);
-                }
-            }
-        } else { // space is occupied
-            return false;
-        }
-
-        // return result of win condition
-        return set.find(LEFT_EDGE) == set.find(RIGHT_EDGE);
+        return play(position, displayNeighbors, Color.BLUE);
     }
 
     public boolean playRed(int position, boolean displayNeighbors) {
+        return play(position, displayNeighbors, Color.RED);
+    }
+
+    public boolean play(int position, boolean displayNeighbors, Color color) {
         // position is an integer for the available hex positions
         if (position < 1 || position > (size * size)) {
             return false;
@@ -68,24 +47,39 @@ public class HexGame {
         // this is for debugging only
         if (displayNeighbors) {
             // the hex positions of neighboring cell are displayed, occupied or not
-            System.out.println(getNeighborsRed(position));
+            if (color == Color.BLUE) {
+                System.out.println(getNeighborsBlue(position));
+            } else {
+                System.out.println(getNeighborsRed(position));
+            }
         }
         // logic for playing
         if (this.colorOfOccupied(position) == Color.NONE) {
-            ArrayList<Integer> neighbors = getNeighborsRed(position);
+            // get correct neighbors
+            ArrayList<Integer> neighbors;
+            if (color == Color.BLUE) {
+                neighbors = getNeighborsBlue(position);
+            } else {
+                neighbors = getNeighborsRed(position);
+            }
+            // iterate over neighbors
             for (Integer neighbor : neighbors) {
-                // only union if neighbor is blue
-                if (this.colorOfOccupied(neighbor) == Color.RED) {
+                // only union if neighbor is the correct color
+                if (this.colorOfOccupied(neighbor) == color) {
                     set.union(position, neighbor);
                 }
             }
-            colorArrayList.set(position, Color.RED);
+            colorArrayList.set(position, color);
         } else { // space is occupied
             return false;
         }
 
         // return result of win condition
-        return set.find(TOP_EDGE) == set.find(BOTTOM_EDGE);
+        if (color == Color.BLUE) {
+            return set.find(LEFT_EDGE) == set.find(RIGHT_EDGE);
+        } else {
+            return set.find(TOP_EDGE) == set.find(BOTTOM_EDGE);
+        }
     }
 
     // NOT REQUIRED ----------------------------------------------------------------------------------------------------
@@ -106,53 +100,21 @@ public class HexGame {
 
     // returns the neighboring cells
     private ArrayList<Integer> getNeighborsRed(int position) {
-        // cover case that position is an edge
-        ArrayList<Integer> neighbors = new ArrayList<>();
-        // init checkers for edge cases
-        boolean topEdge    = size >= position;
-        boolean bottomEdge = size >= (size * size - position);
-        boolean leftEdge   = 1 == (position % size);
-        boolean rightEdge  = 0 == (position % size);
-
-        // size of 1, why are you playing a game this small!
-        if (topEdge && leftEdge && rightEdge && bottomEdge) {
-            return new ArrayList<>();
-        }
-
-        // add neighbors
-        if (!leftEdge) {
-            neighbors.add(position - 1); // left
-        }
-        if (!rightEdge) {
-            neighbors.add(position + 1); // right
-        }
-        if (!topEdge) {
-            neighbors.add(position - 11); // top left
-            if (!rightEdge) {
-                neighbors.add(position - 10); // top right
-            }
-        } else {
-            neighbors.add(TOP_EDGE);
-        }
-        if (!bottomEdge) {
-            neighbors.add(position + 11); // bottom right
-            if (!leftEdge) {
-                neighbors.add(position + 10); // bottom left
-            }
-        } else {
-            neighbors.add(BOTTOM_EDGE);
-        }
-
-        return neighbors;
+        return getNeighbors(position, Color.RED);
     }
+
     private ArrayList<Integer> getNeighborsBlue(int position) {
+        return getNeighbors(position, Color.BLUE);
+    }
+
+    private ArrayList<Integer> getNeighbors(int position, Color color) {
         // cover case that position is an edge
         ArrayList<Integer> neighbors = new ArrayList<>();
         // init checkers for edge cases
-        boolean topEdge    = size >= position;
+        boolean topEdge = size >= position;
         boolean bottomEdge = size >= (size * size - position);
-        boolean leftEdge   = 1 == (position % size);
-        boolean rightEdge  = 0 == (position % size);
+        boolean leftEdge = 1 == (position % size);
+        boolean rightEdge = 0 == (position % size);
 
         // size of 1, why are you playing a game this small!
         if (topEdge && leftEdge && rightEdge && bottomEdge) {
@@ -162,12 +124,12 @@ public class HexGame {
         // add neighbors
         if (!leftEdge) {
             neighbors.add(position - 1); // left
-        } else {
+        } else if (color == Color.BLUE) {
             neighbors.add(LEFT_EDGE);
         }
         if (!rightEdge) {
             neighbors.add(position + 1); // right
-        } else {
+        } else if (color == Color.BLUE) {
             neighbors.add(RIGHT_EDGE);
         }
         if (!topEdge) {
@@ -175,12 +137,16 @@ public class HexGame {
             if (!rightEdge) {
                 neighbors.add(position - 10); // top right
             }
+        } else if (color == Color.RED) {
+            neighbors.add(TOP_EDGE);
         }
         if (!bottomEdge) {
             neighbors.add(position + 11); // bottom right
             if (!leftEdge) {
                 neighbors.add(position + 10); // bottom left
             }
+        } else if (color == Color.RED) {
+            neighbors.add(BOTTOM_EDGE);
         }
 
         return neighbors;
