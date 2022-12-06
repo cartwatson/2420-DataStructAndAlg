a1fb4d268ca70715c0a9c990f83450d9e830ddad
cartwatson <cartwatson@gmail.com>
2022-12-05 18:53:58 -0700

    finished implementation of findMax

--- assign7-MaxFlowMinCut/Graph.java
+++ assign7-MaxFlowMinCut/Graph.java
@@ -12,6 +12,9 @@ public class Graph {
         for (int vertex = 0; vertex < vertexCount; vertex++) {
             vertices[vertex] = new GraphNode(vertex);
         }
+
+        residual = new int[vertices.length][vertices.length];
+
     }
 
     public boolean addEdge(int source, int destination, int capacity) {
@@ -54,6 +57,7 @@ public class Graph {
 
     /** Algorithm to find max-flow in a network */
     public int findMaxFlow(int s, int t, boolean report) {
+        // TODO: abstract reporting (STRETCH GOAL)
         // print header
         if (report) {
             System.out.println("-- Max Flow: " + this.name + " --");
@@ -66,34 +70,64 @@ public class Graph {
             int availableFlow = Integer.MAX_VALUE;
             // v is current vertex
             int v = t;
+            // init path tracker
+            ArrayList<Integer> path = new ArrayList<>();
             // follow the augmenting path from t to s
             while (v != s) {
-                // available flow = min of available flow or residual flow at vertex v
-//                availableFlow = ;
-                // update residual graph
-                for (GraphNode.EdgeInfo e : vertices[v].successor) {
-                    // subtract available flow at vertex v in direction of s to t
+                // calculate residual flow
+                int residualFlow = 0;
+                for (GraphNode.EdgeInfo e : vertices[vertices[v].parent].successor) {
+                    if (e.to == v) {
+                        residualFlow = e.capacity - residual[vertices[v].parent][v];
 
-                    // add available flow at vertex v in direction of t to s
-
+                    }
                 }
-                // update v
+                // available flow = min of available flow or residual flow at vertex v
+                availableFlow = Math.min(availableFlow, residualFlow);
+                // track path
+                path.add(v);
+                // update v to the parent of itself
                 v = vertices[v].parent;
             }
+            path.add(s);
+            // follow the augmenting path from t to s
+            // update residual graph
+            for (int currV = path.size() - 1; currV > 0; currV--) {
+                // subtract available flow at vertex v in direction of s to t
+                residual[path.get(currV - 1)][path.get(currV)] -= availableFlow;
+                // add available flow at vertex v in direction of t to s
+                residual[path.get(currV)][path.get(currV - 1)] += availableFlow;
+            }
+            // print out flow
+            if (report) {
+                System.out.print("Flow "  + availableFlow + ": ");
+                for (int i = path.size() - 1; i >= 0; i--) {
+                    System.out.print(path.get(i) + " ");
+                }
+                System.out.println();
+            }
+
             // add available flow to total flow
             totalFlow += availableFlow;
         }
-        // TODO: abstract reporting (STRETCH GOAL)
-//        // TODO: print out paths & edges & total flow
-//        if (report) {
-//            for (all paths) {
-//                System.out.println("Flow "  + unknown# + ": " + flowAlongPath + " " + verticesAlongPath);
-//            }
-//            for (all edges) {
-//                System.out.println("Edge(" + v1 + ", " + v2 + ") transports " + edge.flow + " items");
-//            }
-//            System.out.println("Total Flow: " + totalFlow);
-//        }
+
+        if (report) {
+            System.out.println();
+            for (int i = 0; i < residual.length; i++) {
+                for (int j = 0; j < residual.length; j++) {
+                    if (residual[i][j] > 0 ) {
+                        System.out.println("Edge(" + i + ", " + j + ") transports " + residual[i][j] + " items");
+                    }
+                }
+            }
+            System.out.println();
+            /** NOTE TO GRADER
+             * not sure if you wanted the totalFlow reporting inside or outside this function...
+             * uncomment the next two lines if totalFlow reporting is to be done inside the graph class
+             */
+            // System.out.printf("Total Flow: %d\n", totalFlow);
+            // System.out.println();
+        }
 
         // return total flow
         return totalFlow;
@@ -101,38 +135,34 @@ public class Graph {
 
     /** Algorithm to find an augmenting path in a network */
     private boolean hasAugmentingPath(int s, int t) {
-        // init queue
-        Queue<Integer> q = new LinkedList<>();
         // reset the vertex that flow comes from on all vertices
         for (GraphNode i : vertices) {
             i.parent = -1;
         }
-        // add s to the queue
+        // init queue
+        Queue<Integer> q = new LinkedList<>();
         q.add(s);
         // while queue is not empty and vertex t does not have a parent
         while (!q.isEmpty() && vertices[t].parent == -1) {
-            // remove from queue as vertex v
             int v = q.remove();
             // for all successor edges from v
             for (GraphNode.EdgeInfo e : vertices[v].successor) {
-                // for the edge, call the other vertex w
                 int w = e.to;
-                // if there is residual capacity from v to w and not already part of the augmenting path, and it isn't vertex s, then it can be used
-                if (e.capacity != 0 && vertices[w].parent != -1 && w != s) {
+                // if (there is residual capacity from v to w) and (not already part of the augmenting path) and (it isn't vertex s) then it can be used
+                if (e.capacity - residual[v][w] != 0 && vertices[w].parent == -1 && w != s) {
                     // remember the path; set parent of w to v
                     vertices[w].parent = v;
-                    // add w to the queue
                     q.add(w);
                 }
             }
         }
-        // if vertex t has a parent: then there is an augmenting path
+        // if vertex t has a parent: an augmenting path exists
         // else: no augmenting path
         return (vertices[t].parent != -1);
     }
 
     /** Algorithm to find the min-cut edges in a network */
-    // added report to parameters
+    // NOTE: added report to parameters
     public void findMinCut(int s, boolean report) {
         // TODO: abstract reporting (STRETCH GOAL)
         // print header
